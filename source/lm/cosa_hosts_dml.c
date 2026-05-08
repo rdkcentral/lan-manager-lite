a3d6d59c3f2b5ae7db60e1eab8f36e0a06ece1ef
Suganya Sugumar <suganya_sugumar@comcast.com>
2026-05-08 05:54:53 +0000

    mesg str fix

--- source/lm/cosa_hosts_dml.c
+++ source/lm/cosa_hosts_dml.c
@@ -879,7 +879,7 @@ Hosts_SetParamStringValue
             strncpy(ssidArray[0], ssidListStr, LM_GEN_STR_SIZE - 1);
             ssidArray[0][LM_GEN_STR_SIZE - 1] = '\0';
             if (!IsNumberString(rssiListStr)) {
-                CcspTraceWarning(("...Inappropriate RSSI value...\n"));
+                CcspTraceWarning((" \n Hosts_SetParamStringValue : < %s : %d > Inappropriate RSSI value in ParamString, invalid token: %s \n",__FUNCTION__,__LINE__, rssiListStr));
                 return FALSE;
             }
             iRssiArray[0] = atoi(rssiListStr);
