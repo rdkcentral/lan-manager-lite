e86501f28429a60cd9137bd6cef3c01348a8fa69
Kanamarlapudi, Venkata Ramana Rao (Contractor) <ramana288@gmail.com>
2026-08-13 12:30:43 -0700

    CIECCPE-694: Test build Reason for change:debug build Test Procedure: verify the logic and functionality Risks: High Priority: P1 Signed-off-by:venkataramanarao_kanamarlapudi@comcast.com Change-Id: I9edb52d8bff9030ba493dd85390dbf1435f601a4

--- source/lm/cosa_wantraffic_api.c
+++ source/lm/cosa_wantraffic_api.c
@@ -1458,7 +1458,6 @@ static VOID* WTC_Thread()
         }
 
         pthread_mutex_lock(&WTCinfo->WanTrafficMutexVar);
-
            ResetIsUpdatedFlag(WanTrafficCountInfo_t[index]->DscpTree);
            WanTrafficCountInfo_t[index]->DscpTree =
                              InsertClient(WanTrafficCountInfo_t[index]->DscpTree, &CliList);
