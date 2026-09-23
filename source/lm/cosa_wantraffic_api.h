712f6453645c7848600d317c02fc9acb72a3892d
R <kr562@apac.comcast.com>
2026-09-23 14:33:26 +0530

    Extend WAN mode initialization retry window

--- source/lm/cosa_wantraffic_api.h
+++ source/lm/cosa_wantraffic_api.h
@@ -107,7 +107,7 @@
  * avoid latching onto the syscfg fallback (DOCSIS/EWAN only) before
  * WanManager has finished marking a PON interface (EPON/XGSPON) active. */
 #define WTC_INIT_WANMODE_RETRY_COUNT     10
-#define WTC_INIT_WANMODE_RETRY_DELAY_SEC 3
+#define WTC_INIT_WANMODE_RETRY_DELAY_SEC 5
 
 #define TR181_COUNTENABLE           "Device.X_RDK_WAN.Interface.1.Stats.DscpCountEnable"
 #define TR181_SLEEPINTERVAL         "Device.X_RDK_WAN.Interface.1.Stats.DscpCountInterval"
