03e626793c44c1573a24a82c843e48653d79667e
mb636 <MathavaAswin_B@comcast.com>
2026-09-25 04:09:56 +0000

    Tighten IPv4 export and ARP handling

--- source/lm/lm_main.c
+++ source/lm/lm_main.c
@@ -1982,23 +1982,20 @@ static enum LM_ADDR_SOURCE _get_addr_source(char *source)
 static void _get_host_ipaddress(LM_host_t *pDestHost, PLmObjectHost pHost)
 {
     int i;
-    int validIPv4Count = 0;
     PLmObjectHostIPAddress pIpSrc; 
-    pDestHost->ipv4AddrAmount = pHost->numIPv4Addr;
+    pDestHost->ipv4AddrAmount = 0;
     pDestHost->ipv6AddrAmount = pHost->numIPv6Addr;
     LM_ip_addr_t *pIp;
     for(i=0, pIpSrc = pHost->ipv4AddrArray; pIpSrc != NULL && i < LM_MAX_IP_AMOUNT;i++, pIpSrc = pIpSrc->pNext){
           if(!pIpSrc->pStringParaValue[LM_HOST_IPAddress_IPAddressId] ||
               pIpSrc->pStringParaValue[LM_HOST_IPAddress_IPAddressId][0] == '\0')
           {
-            pDestHost->ipv4AddrAmount--;
             continue;
           }
-        pIp = &(pDestHost->ipv4AddrList[validIPv4Count]);
+            pIp = &(pDestHost->ipv4AddrList[pDestHost->ipv4AddrAmount]);
           if(inet_pton(AF_INET, pIpSrc->pStringParaValue[LM_HOST_IPAddress_IPAddressId],pIp->addr) != 1)
         {
          CcspTraceWarning(("Invalid IP Address %s\n",pIpSrc->pStringParaValue[LM_HOST_IPAddress_IPAddressId]));
-            pDestHost->ipv4AddrAmount--;
          continue;
         }
         pIp->addrSource = _get_addr_source(pIpSrc->pStringParaValue[LM_HOST_IPAddress_IPAddressSourceId]);
@@ -2007,7 +2004,7 @@ static void _get_host_ipaddress(LM_host_t *pDestHost, PLmObjectHost pHost)
             pIp->LeaseTime = pIpSrc->LeaseTime;
         else
             pIp->LeaseTime = 0;
-        validIPv4Count++;
+        pDestHost->ipv4AddrAmount++;
    }
     
     
@@ -2790,7 +2787,10 @@ static void Hosts_SyncArp (void)
 						  */
 						pIP = Host_AddIPv4Address(pHost, (char *)hosts[i].ipAddr);
 
-                        Host_SetIPAddress(pIP, 0, "NONE");
+                        if (NULL != pIP)
+                        {
+                            Host_SetIPAddress(pIP, 0, "NONE");
+                        }
 
                         _getLanHostComments((char *)hosts[i].phyAddr, comments);
                         if ( comments[0] != 0 )
